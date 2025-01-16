00e452b673a9bfa5e962143a024821e5422d8c8d
ecVictor7 <vb@eco-charge.eu>
2025-01-16 07:36:31 +0100

    version 1.1

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	logger := log.New(os.Stoudt, "[Spooktober] ", log.LstdFlags)
+
+	port := 9000
+
+	logger.Print("Creating guests stroe..")
+	guestDb := store.NewGuestStore(logger)
+	guestDb.AddGuest(store.Guest{Name: "Sigrid", Email: "sig@fake-email.no"})
+
+	srv, err := server.NewServer(logger, port, guestDb)
+	if err != nil {
+		logger.Fatalf("Error when creating server: %s", err)
+		os.Exit(1)
+	}
+	if err := srv.Start(); err != nil {
+		logger.Fatalf("Error when starting server: %s", err)
+		os.Exit(1)
+	}
+}
